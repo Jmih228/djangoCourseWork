ff0ce4ff423d9c1aeda17c8171547139060b6c72
kifusss <kifus_06@mail.ru>
2023-11-25 00:25:42 +0300

    initial commit

--- djangoCourseWork/settings.py
+++ djangoCourseWork/settings.py
@@ -12,8 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-import django
-django.setup()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -45,7 +44,6 @@ INSTALLED_APPS = [
     'clients',
     'logs',
     'mailing'
-
 ]
 
 MIDDLEWARE = [
